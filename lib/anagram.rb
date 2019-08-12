5d57316c18d30369bfca86612c107f2e619c60f1
LaNice Powell <lanicemp@aol.com>
2019-08-12 12:45:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,15 +11,15 @@ class Anagram
   def match(array)
     matches = []
     array.each do |word|
-      if   word.split('').sort == self.word.split('').sort
+      if word.split('').sort == self.word.split('').sort
        matches << word 
        
      #matches << word if word.split('').sort == self.word.split('').sort (this code also works and is a simpler refactored method )
       
       end 
-     end 
-   matches 
     end 
+   matches 
+  end 
     
 
 
